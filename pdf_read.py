fc43893e68f7405488edb9436a7ba08336c4e536
Sanjay-E22 <sanjayelangovan22@gmail.com>
2026-03-14 20:28:46 +0530

    new

--- pdf_read.py
+++ pdf_read.py
@@ -3,7 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-
+ 
 
 
 # 1. Load and Split
